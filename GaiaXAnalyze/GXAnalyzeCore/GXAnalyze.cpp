b058bf5823c6c6ee99b4e70bde404c4bc52a54f4
UncleChan <a18475180066@gamil.com>
2022-04-11 19:46:51 +0800

    bugfix

--- GaiaXAnalyze/GXAnalyzeCore/GXAnalyze.cpp
+++ GaiaXAnalyze/GXAnalyzeCore/GXAnalyze.cpp
@@ -862,7 +862,7 @@ static GXValue pointer;
 long GXAnalyze::getValue(string expression, void *source) {
     char *input;
     int inputLength = expression.length();
-    input = new char[inputLength+2];
+    input = new char[inputLength + 2];
     vector<GXATSNode> array;
     array.reserve(128);
     string result = "#";
@@ -916,12 +916,14 @@ long GXAnalyze::check(string s, vector<GXATSNode> array, void *p_analyze, void *
         //当前new_status,下一入栈的新状态
         new_status = tableT[m];
         if (new_status == "acc") {
-            if(pointer.tag == GX_TAG_STRING){
-                delete []pointer.u.str;
-                pointer.u.str = nullptr;
-            }
             if (valueStack[0].token == "string") {
                 const char *tem = valueStack[0].name.c_str();
+                if (pointer.tag == GX_TAG_STRING) {
+                    if (pointer.u.str != nullptr) {
+                        delete[]pointer.u.str;
+                        pointer.u.str = nullptr;
+                    }
+                }
                 pointer = GX_NewGXString(tem);
             } else if (valueStack[0].token == "bool") {
                 if (valueStack[0].name == "true") {

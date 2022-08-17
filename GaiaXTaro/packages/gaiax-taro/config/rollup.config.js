b392235dbd49409c8ddf062212a39d86b69e5813
biezhihua <biezhihua@gmail.com>
2022-08-17 11:55:01 +0800

    feat:

--- GaiaXTaro/packages/gaiax-taro/config/rollup.config.js
+++ GaiaXTaro/packages/gaiax-taro/config/rollup.config.js
@@ -1,17 +1,36 @@
 import typescript from 'rollup-plugin-typescript2';
+import RollupCopy from 'rollup-plugin-copy'
+import NodePath from 'path'
+import Package from '../package.json'
+
+const resolveFile = path => NodePath.resolve(__dirname, '..', path)
 
 export default {
-    input: ["src/index.ts"],
+    input: [resolveFile(Package.source)],
     output: [
         {
-            dir: "dist",
-            entryFileNames: "[name].js",
-            format: "cjs",
-            exports: "named"
+            file: resolveFile(Package.main),
+            format: 'cjs',
+            sourcemap: true
         }
     ],
     plugins: [
         typescript(),
+        RollupCopy({
+            targets: [
+                {
+                    src: resolveFile('src/style'),
+                    dest: resolveFile('dist')
+                }
+            ]
+        })
     ],
-    external: ["react"]
+    external: [
+        'react',
+        'react-dom',
+        '@tarojs/components',
+        '@tarojs/runtime',
+        '@tarojs/taro',
+        '@tarojs/react'
+    ]
 };

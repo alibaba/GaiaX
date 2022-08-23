5e1623e4696a98d747e88024628615899a3b3239
biezhihua <biezhihua@gmail.com>
2022-08-23 17:12:39 +0800

    feat:

--- GaiaXTaro/packages/gaiax-taro/src/gaiax/GXCssConvertStyle.tsx
+++ GaiaXTaro/packages/gaiax-taro/src/gaiax/GXCssConvertStyle.tsx
@@ -115,7 +115,9 @@ export default class GXCssConvertStyle {
         gxParentNode?: GXNode
     ) {
 
+        //
         // Layout
+        // 
 
         let display = srcCss['display'];
         if (display != undefined) {
@@ -273,159 +275,16 @@ export default class GXCssConvertStyle {
             targetStyle.maxHeight = maxHeight;
         }
 
-         // 特殊处理：在微信小程序上不生效;在H5上生效
+        // 特殊处理：在微信小程序上不生效;在H5上生效
         // 小程序不支持
         let aspectRatio = srcCss['aspect-ratio'];
         if (aspectRatio != undefined) {
             targetStyle.aspectRatio = aspectRatio;
         }
 
-        // 对图片进行特殊处理
-        if (gxTemplateNode.isImageType()) {
-            // 特殊处理：图片不允许被压缩
-            targetStyle.flexShrink = '0';
-        }
-
-        // 对View的特殊处理
-        if (gxTemplateNode.isViewType() || gxTemplateNode.isGaiaTemplate()) {
-            // 特殊处理：如果是嵌套节点，并且自身有内边距，并且width100%，那么需要处理一下
-            if ((paddingLeft != null || paddingRight != null) && targetStyle.width == '100%' && gxParentNode != null) {
-                targetStyle.width = 'auto';
-                targetStyle.flexGrow = '1';
-            }
-        }
-
-        // 特殊处理：对文字自适应的处理
-        let fitContent = srcCss['fit-content'];
-        // 对文字进行特殊处理
-        if (gxTemplateNode.isTextType() && fitContent != null && fitContent != undefined) {
-            // 特殊处理：如果横向，文字是固定宽度，那么不能被压缩
-            if (gxParentNode?.gxTemplateNode?.finalStyle?.flexDirection == 'row' && width != undefined) {
-                targetStyle.flexShrink = '0';
-            }
-
-            // 特殊处理：如果竖向, 文字是固定高度，那么不能被压缩
-            if (gxParentNode?.gxTemplateNode?.finalStyle?.flexDirection == 'column' && height != undefined) {
-                targetStyle.flexShrink = '0';
-            }
-
-
-            // 如果宽度是auto并且设置的自增长，那么fitcontent之后，需要按照实际的宽度设置
-            if ((width == 'auto' || width == undefined) && flexGrow == '1' && (fitContent == 'true' || fitContent == true)) {
-                targetStyle.flexGrow = '0';
-            }
-            // 特殊处理：如果宽度是具体的像素值，并且设置了fitcontent，那么需要宽度auto
-            else if (width != undefined && width.endsWith('px') && (fitContent == 'true' || fitContent == true)) {
-                targetStyle.width = 'auto';
-            }
-        }
-
+        //
         // Style 
-
-        switch (srcLayer.type) {
-            case 'gaia-template':
-                // 如果是嵌套模板的根节点，并且宽度100%，有左右padding信息，那么需要可以被压缩
-                if (gxTemplateContext.isNestChildTemplate && srcLayer['sub-type'] == undefined) {
-                    if (width == '100%' && (paddingLeft != undefined || paddingRight != undefined)) {
-                        targetStyle.flexShrink = '1';
-                        // 若还是垂直布局，那么宽度需要auto
-                        if (gxParentNode?.gxTemplateNode.finalStyle?.flexDirection == 'column') {
-                            targetStyle.width = 'auto';
-                        }
-                    }
-                }
-                break;
-            case 'view': break;
-            case 'text':
-            case 'richtext':
-            case 'iconfont':
-                let fontSize = srcCss['font-size'];
-                if (fontSize != undefined) {
-                    targetStyle.fontSize = fontSize;
-                }
-
-                let fontFamily = srcCss['font-family'];
-                if (fontFamily != undefined) {
-                    targetStyle.fontFamily = fontFamily;
-                }
-
-                let fontWeight = srcCss['font-weight'];
-                if (fontWeight != undefined) {
-                    targetStyle.fontWeight = fontWeight;
-                }
-
-                let color = srcCss['color'];
-                if (color != undefined) {
-                    targetStyle.color = color;
-                }
-
-                let textOverflow = srcCss['text-overflow'];
-                targetStyle.textOverflow = "ellipsis"
-                if (textOverflow != undefined) {
-                    targetStyle.textOverflow = textOverflow;
-                }
-
-                let textAlign = srcCss['text-align'];
-                if (textAlign != undefined) {
-                    targetStyle.textAlign = textAlign;
-                }
-
-                let lineHeight = srcCss['line-height'];
-                if (lineHeight != undefined) {
-                    targetStyle.lineHeight = lineHeight;
-                }
-
-                // 特殊处理：文字默认居中
-                if (height != undefined && height.endsWith('px')) {
-                    targetStyle.lineHeight = height;
-                }
-
-                let textDecoration = srcCss['text-decoration'];
-                if (textDecoration != undefined) {
-                    targetStyle.textDecoration = textDecoration;
-                }
-
-                // 特殊处理：处理多行文字...逻辑
-                let maxLines = srcCss['lines'];
-                if (maxLines != undefined && maxLines > 1) {
-                    targetStyle['-webkit-box-orient'] = 'vertical';
-                    targetStyle['-webkit-line-clamp'] = maxLines;
-                    targetStyle.display = '-webkit-box';
-
-                    // 如果不是fitcontent=true，并且多行，那么需要手动计算一下高度，并且赋值
-                    if (targetStyle.height != undefined) {
-                        let tmpHeight = targetStyle.height as string;
-                        if (tmpHeight.endsWith('px')) {
-                            targetStyle.height = parseInt(tmpHeight.substring(0, tmpHeight.indexOf('px'))) * maxLines + 'px';
-                        }
-                        // 如果不符合后缀逻辑，那么直接设置auto
-                        else {
-                            targetStyle.height = 'auto';
-                        }
-                    }
-                    // 自适应，那么高度设置成auto
-                    else if (fitContent == 'true' || fitContent == true) {
-                        targetStyle.height = 'auto';
-                    }
-
-                }
-                // 特殊处理：处理单行文字...逻辑
-                else if (targetStyle.textOverflow == "ellipsis") {
-                    targetStyle.minWidth = '0px';
-                    targetStyle.whiteSpace = 'nowrap';
-                    targetStyle.display = '';
-                }
-
-                break;
-            case 'grid': break;
-            case 'scroll': break;
-            case 'image':
-                let mode = srcCss['mode'];
-                if (mode != undefined) {
-                    targetStyle.mode = mode;
-                }
-                break;
-        }
+        //
 
         let backgroundColor = srcCss['background-color'];
         if (backgroundColor != undefined) {
@@ -471,5 +330,145 @@ export default class GXCssConvertStyle {
         if (borderRadius != undefined) {
             targetStyle.borderRadius = borderRadius;
         }
+
+        if (gxTemplateNode.isTextType() || gxTemplateNode.isRichTextType() || gxTemplateNode.isIconFontType()) {
+            let fontSize = srcCss['font-size'];
+            if (fontSize != undefined) {
+                targetStyle.fontSize = fontSize;
+            }
+
+            let fontFamily = srcCss['font-family'];
+            if (fontFamily != undefined) {
+                targetStyle.fontFamily = fontFamily;
+            }
+
+            let fontWeight = srcCss['font-weight'];
+            if (fontWeight != undefined) {
+                targetStyle.fontWeight = fontWeight;
+            }
+
+            let color = srcCss['color'];
+            if (color != undefined) {
+                targetStyle.color = color;
+            }
+
+            let textOverflow = srcCss['text-overflow'];
+            targetStyle.textOverflow = "ellipsis"
+            if (textOverflow != undefined) {
+                targetStyle.textOverflow = textOverflow;
+            }
+
+            let textAlign = srcCss['text-align'];
+            if (textAlign != undefined) {
+                targetStyle.textAlign = textAlign;
+            }
+
+            let lineHeight = srcCss['line-height'];
+            if (lineHeight != undefined) {
+                targetStyle.lineHeight = lineHeight;
+            }
+
+            let textDecoration = srcCss['text-decoration'];
+            if (textDecoration != undefined) {
+                targetStyle.textDecoration = textDecoration;
+            }
+        }
+        if (gxTemplateNode.isImageType()) {
+            let mode = srcCss['mode'];
+            if (mode != undefined) {
+                targetStyle.mode = mode;
+            }
+        }
+
+        //
+        // 特殊处理的逻辑
+        // 
+
+        // 对图片进行处理
+        if (gxTemplateNode.isImageType()) {
+            // 特殊处理：图片不允许被压缩
+            targetStyle.flexShrink = '0';
+        }
+
+        // 对View进行处理
+        if (gxTemplateNode.isViewType() || gxTemplateNode.isGaiaTemplate()) {
+            // 特殊处理：如果是嵌套节点，并且自身有内边距，并且width100%，那么需要处理一下
+            if ((paddingLeft != null || paddingRight != null) && targetStyle.width == '100%' && gxParentNode != null) {
+                targetStyle.width = 'auto';
+                targetStyle.flexGrow = '1';
+            }
+        }
+
+        // 对Text进行处理
+        if (gxTemplateNode.isTextType()) {
+            // 特殊处理：对文字自适应的处理
+            let fitContent = srcCss['fit-content'];
+
+            // 对文字进行特殊处理
+            if (fitContent != null && fitContent != undefined) {
+
+                // 特殊处理：如果横向，文字是固定宽度，那么不能被压缩
+                if (gxParentNode?.gxTemplateNode?.finalStyle?.flexDirection == 'row' && width != undefined) {
+                    targetStyle.flexShrink = '0';
+                }
+
+                // 特殊处理：如果竖向, 文字是固定高度，那么不能被压缩
+                if (gxParentNode?.gxTemplateNode?.finalStyle?.flexDirection == 'column' && height != undefined) {
+                    targetStyle.flexShrink = '0';
+                }
+
+                // 如果宽度是auto并且设置的自增长，那么fitcontent之后，需要按照实际的宽度设置
+                if ((width == 'auto' || width == undefined) && flexGrow == '1' && (fitContent == 'true' || fitContent == true)) {
+                    targetStyle.flexGrow = '0';
+                }
+                // 特殊处理：如果宽度是具体的像素值，并且设置了fitcontent，那么需要宽度auto
+                else if (width != undefined && width.endsWith('px') && (fitContent == 'true' || fitContent == true)) {
+                    targetStyle.width = 'auto';
+                }
+            }
+        }
+
+        if (gxTemplateNode.isTextType() || gxTemplateNode.isRichTextType() || gxTemplateNode.isIconFontType()) {
+            
+            // 特殊处理：对文字自适应的处理
+            let fitContent = srcCss['fit-content'];
+
+            // 特殊处理：文字默认居中
+            if (height != undefined && height.endsWith('px')) {
+                targetStyle.lineHeight = height;
+            }
+
+            // 特殊处理：处理多行文字...逻辑
+            let maxLines = srcCss['lines'];
+            if (maxLines != undefined && maxLines > 1) {
+                targetStyle['-webkit-box-orient'] = 'vertical';
+                targetStyle['-webkit-line-clamp'] = maxLines;
+                targetStyle.display = '-webkit-box';
+
+                // 如果不是fitcontent=true，并且多行，那么需要手动计算一下高度，并且赋值
+                if (targetStyle.height != undefined) {
+                    let tmpHeight = targetStyle.height as string;
+                    if (tmpHeight.endsWith('px')) {
+                        targetStyle.height = parseInt(tmpHeight.substring(0, tmpHeight.indexOf('px'))) * maxLines + 'px';
+                    }
+                    // 如果不符合后缀逻辑，那么直接设置auto
+                    else {
+                        targetStyle.height = 'auto';
+                    }
+                }
+                // 自适应，那么高度设置成auto
+                else if (fitContent == 'true' || fitContent == true) {
+                    targetStyle.height = 'auto';
+                }
+
+            }
+            // 特殊处理：处理单行文字...逻辑
+            else if (targetStyle.textOverflow == "ellipsis") {
+                targetStyle.minWidth = '0px';
+                targetStyle.whiteSpace = 'nowrap';
+                targetStyle.display = '';
+            }
+        }
+
     }
 }

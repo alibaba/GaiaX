f53ed29396a6ddc380b3fbb7a345cf7d7a13c765
biezhihua <biezhihua@gmail.com>
2022-10-20 16:32:04 +0800

    feat: reactor fastpreview module

---
+++ GaiaXAndroidDemo/app/src/main/kotlin/com/alibaba/gaiax/demo/source/GXFastPreviewSource.kt
@@ -0,0 +1,32 @@
+package com.alibaba.gaiax.demo.source
+
+import com.alibaba.fastjson.JSONObject
+import com.alibaba.gaiax.GXRegisterCenter
+import com.alibaba.gaiax.GXTemplateEngine
+import com.alibaba.gaiax.data.cache.GXTemplateInfoSource
+import com.alibaba.gaiax.template.GXTemplate
+import com.alibaba.gaiax.template.GXTemplateKey
+
+class GXFastPreviewSource : GXRegisterCenter.GXIExtensionTemplateSource {
+
+    companion object {
+        val instance by lazy {
+            GXFastPreviewSource()
+        }
+    }
+
+    private val cache = mutableMapOf<String, GXTemplate>()
+
+    override fun getTemplate(templateItem: GXTemplateEngine.GXTemplateItem): GXTemplate? {
+        return cache[templateItem.templateId]
+    }
+
+    fun addTemplate(templateId: String, data: JSONObject) {
+        GXTemplateInfoSource.instance.clean()
+        val layer = data.getString(GXTemplateKey.GAIAX_INDEX_JSON) ?: ""
+        val css = data.getString(GXTemplateKey.GAIAX_INDEX_CSS) ?: ""
+        val dataBind = data.getString(GXTemplateKey.GAIAX_INDEX_DATABINDING) ?: ""
+        val js = data.getString(GXTemplateKey.GAIAX_INDEX_JS) ?: ""
+        cache[templateId] = GXTemplate(templateId, "fastpreview", -1, layer, css, dataBind, js)
+    }
+}

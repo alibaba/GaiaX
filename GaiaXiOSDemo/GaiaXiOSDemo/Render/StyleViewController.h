1830a4b177120552ff173e559607b236cbc91d54
biezhihua <biezhihua@gmail.com>
2022-12-08 16:43:36 +0800

    feat: Add blur example for Demo project

---
+++ GaiaXiOSDemo/GaiaXiOSDemo/Render/StyleViewController.h
@@ -0,0 +1,16 @@
+//
+//  StyleViewController.h
+//  GaiaXiOSDemo
+//
+//  Created by biezhihua on 2022/12/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StyleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

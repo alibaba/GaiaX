e06f222820c8d950511c9b27c1e686be6f9e2a21
biezhihua <biezhihua@gmail.com>
2023-02-08 19:02:51 +0800

    fix edgeinsets

--- GaiaXAndroid/src/main/kotlin/com/alibaba/gaiax/template/GXLayer.kt
+++ GaiaXAndroid/src/main/kotlin/com/alibaba/gaiax/template/GXLayer.kt
@@ -106,6 +106,7 @@ data class GXLayer constructor(
             data: JSONObject
         ): GXLayer {
             val direction = data.getString(GXTemplateKey.GAIAX_LAYER_DIRECTION)
+            val edgeInsets = data.getString(GXTemplateKey.GAIAX_LAYER_EDGE_INSETS)
             var itemSpacing = data.getString(GXTemplateKey.GAIAX_LAYER_ITEM_SPACING)
             if (itemSpacing == null) {
                 itemSpacing = data.getString(GXTemplateKey.GAIAX_LAYER_LINE_SPACING)
@@ -132,6 +133,7 @@ data class GXLayer constructor(
                     scrollConfig = GXScrollConfig.create(
                         data,
                         direction,
+                        edgeInsets,
                         itemSpacing,
                         gravity
                     ),

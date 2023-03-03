b8bfbdc4fda9cba302b543db61743b42fba438d3
biezhihua <biezhihua@gmail.com>
2023-03-03 17:21:14 +0800

    copy

---
+++ GaiaXAndroidDemo/settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "GaiaXAndroidDemo"
+
+include(":app")
+
+include(":GXAnalyzeAndroid")
+project(":GXAnalyzeAndroid").projectDir = file("../GaiaXAnalyze/GXAnalyzeAndroid")
+
+include(":GaiaXAndroid")
+project(":GaiaXAndroid").projectDir = file("../GaiaXAndroid")
+
+include(":GaiaXAndroidAdapter")
+project(":GaiaXAndroidAdapter").projectDir = file("../GaiaXAndroidAdapter")
+
+include(":GaiaXAndroidClientToStudio")
+project(":GaiaXAndroidClientToStudio").projectDir = file("../GaiaXAndroidClientToStudio")
+
+include(":GaiaXAndroidMicrobenchmark")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}

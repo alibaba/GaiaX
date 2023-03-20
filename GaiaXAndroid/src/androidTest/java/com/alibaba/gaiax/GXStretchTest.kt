f725adbee0b216127b28a03b0b2217f85ff6fca6
biezhihua <biezhihua@gmail.com>
2023-03-20 13:09:59 +0800

    fix multi thread free style

--- GaiaXAndroid/src/androidTest/java/com/alibaba/gaiax/GXStretchTest.kt
+++ GaiaXAndroid/src/androidTest/java/com/alibaba/gaiax/GXStretchTest.kt
@@ -8,6 +8,7 @@ import com.alibaba.gaiax.utils.*
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
 
 
 @RunWith(AndroidJUnit4::class)
@@ -26,4 +27,42 @@ class GXStretchTest : GXBaseTest() {
 
         Assert.assertEquals(false, safeSetStyle)
     }
+
+    @Test
+    fun multi_thread_node_set_style_null_pointer() {
+        Stretch.init()
+
+        val c1 = CountDownLatch(1)
+        val c2 = CountDownLatch(1)
+        val c3 = CountDownLatch(1)
+        var safeSetStyle = false
+        val style2 = Style()
+
+        Thread {
+            c2.await()
+            Thread {
+                style2.safeFree()
+            }.start()
+            Thread {
+                c1.countDown()
+            }.start()
+        }.start()
+
+        Thread {
+            val style1 = Style()
+            style1.safeInit()
+            val node = Node("node", style1)
+            style2.safeInit()
+            safeSetStyle = node.safeSetStyle(style2) {
+                c2.countDown()
+                c1.await()
+                c3.countDown()
+            }
+        }.start()
+
+        c3.await()
+
+        Assert.assertEquals(true, safeSetStyle)
+    }
+
 }

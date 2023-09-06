26fbb5ceae5186472ebbd6ecdacbedb1a5176755
biezhihua <biezhihua@gmail.com>
2023-09-06 13:26:30 +0800

    fix script

--- android_local_publish.sh
+++ android_local_publish.sh
@@ -4,27 +4,27 @@ CUR=`pwd`
 ###########
 echo "Publish GaiaX ClientToStudio"
 cd $CUR/GaiaXAndroidClientToStudio
-./gradlew clean build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
-cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX-ClientToStudio
+./gradlew build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
+cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX-ClientToStudio/main-SNAPSHOT
 ls -ll
 
 ###########
 echo "Publish GaiaX Analyze"
 cd $CUR/GaiaXAnalyze/GXAnalyzeAndroid
-./gradlew clean build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
-cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX-Analyze
+./gradlew build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
+cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX-Analyze/main-SNAPSHOT
 ls -ll
 
 ###########
 echo "Publish GaiaX"
 cd $CUR/GaiaXAndroid
-./gradlew clean build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
-cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX
+./gradlew build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
+cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX/main-SNAPSHOT
 ls -ll
 
 ###########
 echo "Publish GaiaX Adapter"
 cd $CUR/GaiaXAndroidAdapter
-./gradlew clean build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
-cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX-Adapter
+./gradlew build -Pgroup=com.github.alibaba.GaiaX -Pversion=main-SNAPSHOT -xtest -xlint publishToMavenLocal
+cd ~/.m2/repository/com/github/alibaba/GaiaX/GaiaX-Adapter/main-SNAPSHOT
 ls -ll

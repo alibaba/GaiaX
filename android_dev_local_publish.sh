883bd606b4eec38023bc10d28ff6e4f8b72b4219
biezhihua <biezhihua@gmail.com>
2023-10-11 16:57:28 +0800

    fix android build

--- android_dev_local_publish.sh
+++ android_dev_local_publish.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 CUR=`pwd`
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GXAnalyzeAndroid"
 cd $CUR/GaiaXAnalyze/GXAnalyzeAndroid
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroid"
 cd $CUR/GaiaXAndroid
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroidAdapter"
 cd $CUR/GaiaXAndroidAdapter
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroidClientToStudio"
 cd $CUR/GaiaXAndroidClientToStudio
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroidQuickJS"
 cd $CUR/GaiaXAndroidQuickJS
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroidJS"
 cd $CUR/GaiaXAndroidJS
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroidJSAdapter"
 cd $CUR/GaiaXAndroidJSAdapter
-chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-20231011-SNAPSHOT" -xtest -xlint publishToMavenLocal
+chmod +rx ./gradlew && ./gradlew assembleDebug clean assembleDebug -Ptype="debug" -Pgroup="com.github.alibaba.GaiaX" -Pversion="dev-SNAPSHOT" -xtest -xlint publishToMavenLocal
 
-echo ##########################################################################
+echo "##########################################################################"
 echo "Publish GaiaXAndroidDemo"
 cd $CUR/GaiaXAndroidDemo
 chmod +rx ./gradlew && ./gradlew build

a0de97d9e4f86355237c326431af0622b1a7250f
biezhihua <biezhihua@gmail.com>
2023-12-14 15:23:37 +0800

    remove unused code

--- harmony/src/lib.rs
+++ harmony/src/lib.rs
@@ -1,14 +1,3 @@
-// //! #[cxx::bridge]
-// #[cxx::bridge]
-// mod ffi{
-//     extern "Rust"{
-//         fn print_message_in_rust();
-//     }
-// }
-
-// fn print_message_in_rust(){
-//     println!("Here is a test for cpp call Rust.");
-// }
 #[no_mangle]
 pub extern "C" fn hello_from_rust() {
     println!("Hello from Rust!");

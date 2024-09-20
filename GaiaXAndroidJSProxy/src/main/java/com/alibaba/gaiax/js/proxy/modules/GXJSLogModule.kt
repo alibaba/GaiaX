68a9ed77386c755bdce165ccfcfa7c45aeb250cd
biezhihua <biezhihua@gmail.com>
2024-09-20 15:51:24 +0800

    调整build和日志输出

--- GaiaXAndroidJSProxy/src/main/java/com/alibaba/gaiax/js/proxy/modules/GXJSLogModule.kt
+++ GaiaXAndroidJSProxy/src/main/java/com/alibaba/gaiax/js/proxy/modules/GXJSLogModule.kt
@@ -8,7 +8,6 @@ import com.alibaba.gaiax.js.api.GXJSBaseModule
 import com.alibaba.gaiax.js.api.annotation.GXSyncMethod
 
 
-
 @Keep
 class GXJSLogModule : GXJSBaseModule() {
 
@@ -22,18 +21,22 @@ class GXJSLogModule : GXJSBaseModule() {
         }
 
         fun sendJSLogMsg(level: String, msg: String) {
-            val data = JSONObject()
-            data["jsonrpc"] = "2.0"
-            data["method"] = "js/console"
-            val params = JSONObject()
-            params["level"] = level
-            params["data"] = msg
-            data["params"] = params
             try {
-                if (com.alibaba.gaiax.js.utils.Log.isLog()) {
-                    com.alibaba.gaiax.js.utils.Log.d("sendJSLogMsg() called with: $data")
+                GXJSEngine.instance.getSocketSender()?.let {
+                    val data = JSONObject()
+                    data["jsonrpc"] = "2.0"
+                    data["method"] = "js/console"
+                    val params = JSONObject()
+                    params["level"] = level
+                    params["data"] = msg
+                    data["params"] = params
+
+                    if (com.alibaba.gaiax.js.utils.Log.isLog()) {
+                        com.alibaba.gaiax.js.utils.Log.d("sendJSLogMsg() called with: $data")
+                    }
+
+                    it.onSendMsg(data)
                 }
-                GXJSEngine.instance.getSocketSender()?.onSendMsg(data)
             } catch (e: Exception) {
                 if (com.alibaba.gaiax.js.utils.Log.isLog()) {
                     com.alibaba.gaiax.js.utils.Log.d("sendJSLogMsg() called with: ${e.message}")
@@ -47,28 +50,28 @@ class GXJSLogModule : GXJSBaseModule() {
     fun log(data: String) {
         val msg = getLogMsg(data)
         sendJSLogMsg("log", msg)
-        Log.d(TAG, "jslog:msg = $msg")
+        Log.d(TAG, "log() called with: msg = $msg")
     }
 
     @GXSyncMethod
     fun info(data: String) {
         val msg = getLogMsg(data)
         sendJSLogMsg("info", msg)
-        Log.i(TAG, "jslog:msg = $msg")
+        Log.i(TAG, "info() called with: msg = $msg")
     }
 
     @GXSyncMethod
     fun warn(data: String) {
         val msg = getLogMsg(data)
         sendJSLogMsg("warn", msg)
-        Log.w(TAG, "jslog:msg = $msg")
+        Log.w(TAG, "warn() called with: msg = $msg")
     }
 
     @GXSyncMethod
     fun error(data: String) {
         val msg = getLogMsg(data)
         sendJSLogMsg("error", msg)
-        Log.e(TAG, "jslog:msg = $msg")
+        Log.e(TAG, "error() called with: msg = $msg")
     }
 
     override val name: String
